88c7fa9d7b6bba1c7326dbe3f90537f676f0a80b
myoaung <myo@gtmi.co.jp>
2023-12-31 18:55:43 +0900

    fix message reply format

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -59,14 +59,12 @@ let getWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-  let response = { text: "hello world!" };
+  let response = "";
 
   // Check if the message contains text
   if (received_message.text) {
     // Create the payload for a basic text message
-    response = {
-      text: `You sent the message: "${received_message.text}". Now send me an image!`,
-    };
+    response = `You sent the message: "${received_message.text}". Now send me an image!`;
   } else if (received_message.attachments) {
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
