a713b8de2b3b00a60d36f34db1a02227c4ec3c5d
myoaung <myo@gtmi.co.jp>
2024-01-02 14:05:48 +0900

    adding missing fs

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 import request from "request";
 
-const verses = loadVerses();
 const fs = require("fs");
+const verses = loadVerses();
 
 let postWebhook = (req, res) => {
   // Parse the request body from the POST
